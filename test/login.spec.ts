524a484da3a51b7c0fb0466475edafd03db7b34c
Petar Ginev <pginev7@gmail.com>
2026-08-03 18:23:04 +0300

    TC2 Automated

--- test/login.spec.ts
+++ test/login.spec.ts
@@ -1,6 +1,11 @@
 import { test } from "@playwright/test";
 import TC1 from "./testCases/TC1";
+import TC2 from "./testCases/TC2";
 
 test("TC1: Verify successful login with valid standard_user credentials", async ({page}) => {
     await TC1(page);
+});
+
+test("TC2: Verify the Login button can be activated using the Enter key", async ({page}) => {
+    await TC2(page);
 });
---
+++ test/testCases/TC2.ts
@@ -0,0 +1,29 @@
+import { expect, Page } from "@playwright/test";
+import { ID_USERNAME, ID_PASSWORD, STANDARD_USERNAME, STANDARD_PASSWORD, ID_LOGIN_BTN } from "../helpers/login";
+
+/* TC1: Verify the Login button can be activated using the Enter key.
+* Test Steps:
+1. Navigate to the the login page.
+2. Enter valid username in the Username field.
+3. Enter valid password in the Password field.
+4. Press the Enter key.
+
+* Expected result:
+1. Login page is displayed.
+2. Username is entered successfully.
+3. Password is entered successfully.
+4. User is redirected to the Products page.
+*/
+
+export default async function (page: Page) {
+    // 1
+    await page.goto('/');
+
+    // 2 3
+    await page.fill(ID_USERNAME, STANDARD_USERNAME);
+    await page.fill(ID_PASSWORD, STANDARD_PASSWORD);
+
+    // 4
+    await page.locator(ID_LOGIN_BTN).press("Enter");
+    await expect(page).toHaveURL(/inventory/);
+}
