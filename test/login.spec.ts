0915310b50a6ec611a747370dc33f1ec59b7d63a
Petar Ginev <pginev7@gmail.com>
2026-08-03 19:29:26 +0300

    TC4 Automated

--- test/login.spec.ts
+++ test/login.spec.ts
@@ -2,6 +2,7 @@ import { test } from "@playwright/test";
 import TC1 from "./testCases/TC1";
 import TC2 from "./testCases/TC2";
 import TC3 from "./testCases/TC3";
+import TC4 from "./testCases/TC4";
 
 test("TC1: Verify successful login with valid standard_user credentials", async ({page}) => {
     await TC1(page);
@@ -13,4 +14,8 @@ test("TC2: Verify the Login button can be activated using the Enter key", async 
 
 test("TC3: Verify login with valid username and invalid password", async ({page}) => {
     await TC3(page);
+});
+
+test("TC4: Verify login with invalid username and valid password", async ({page}) => {
+    await TC4(page);
 });
